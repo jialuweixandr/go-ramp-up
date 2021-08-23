7a7e8de207b2fc39b99fc4d224ecd997e1a478ca
jialuwei <jialu.wei@xandr.com>
2021-08-23 17:27:20 -0400

    working unit tests... the whole thing is kinda messy though

--- jokes/joke_test.go
+++ jokes/joke_test.go
@@ -4,8 +4,56 @@ import (
 	"testing"
 )
 
-func TestGetARandomJoke(t *testing.T) {
-	if 1 != 1 {
-		t.Fatalf("This is bad..")
+func TestGetARandomJokeType(t *testing.T) {
+	joke_type := "programming"
+	ch := make(chan JokeResult)
+	go GetARandomJoke(ch, joke_type) // Question: why were you blocking when I omitted "go" keyword??
+	jkres := <- ch
+
+	if jkres.Type != joke_type ||  jkres.Error != nil {
+		t.Fatalf("Either wrong joke type or error somewhere down the line...Expected: %v, Actual: %v", joke_type, jkres.Type) // TODO
+	}
+}
+
+
+// TODO: maybe check the exact error text? What's this error..?
+func TestGetARandomJokeEmpty(t *testing.T) {
+	joke_type := ""
+	ch := make(chan JokeResult)
+	go GetARandomJoke(ch, joke_type) // Question: why were you blocking when I omitted "go" keyword??
+	jkres := <- ch
+
+	if jkres.Error == nil {
+		t.Fatalf("Expect error with empty joke_type string. Actual: No error.")
+	}
+}
+
+
+func TestGetRandomJokes(t *testing.T) {
+	joke_type := "general"
+	num_jokes := 3
+
+	var jkres_list, error = GetRandomJokes(num_jokes, joke_type)
+
+	if len(jkres_list) != num_jokes || error != nil {
+		t.Fatalf("Unexpected result length. Expected: %v, Actual: %v", len(jkres_list), num_jokes)
+	}
+
+	for _, jkres := range jkres_list {
+		if jkres.Type != joke_type ||  jkres.Error != nil {
+			t.Fatalf("Either wrong joke type or error somewhere down the line...Expected: %v, Actual: %v", joke_type, jkres.Type) // TODO
+		}
+	}
+}
+
+// TODO: what would happen if joke_type is bad?
+func TestGetRandomJokesBad(t *testing.T) {
+	joke_type := "general"
+	num_jokes := -1
+
+	var _, error = GetRandomJokes(num_jokes, joke_type)
+
+	if error == nil{
+		t.Fatalf("Expect error. Actual: No error.")
 	}
 }
--- jokes/jokes.go
+++ jokes/jokes.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 	"os"
 	"sync"
+	"errors"
 )
  
 
@@ -39,14 +40,19 @@ func (c *Cache) checkVisited(id int) bool {
 }
 
 
+// TODO: make errors consistent
 // call the joke api and return
 func GetARandomJoke(ch chan JokeResult, joke_type string) {
+	if joke_type != "programming" && joke_type != "general" {
+		//fmt.Println("Bad joke type! Shold be programming or general")
+		ch <- JokeResult{Error: errors.New("Bad joke type! Shold be programming or general")}
+	}
+
 	// return value
 	var jkres JokeResult = JokeResult{}
 
 	// build url with query
 	//query := "general"
-	// TODO: put an error check for "type"
 	path := fmt.Sprintf("https://official-joke-api.appspot.com/jokes/%s/random", joke_type)
 	//fmt.Println("path: ", path)
 
@@ -74,25 +80,37 @@ func GetARandomJoke(ch chan JokeResult, joke_type string) {
 	// json parsing
 	var jkres_arr []JokeResult
 	json.Unmarshal(responseData, &jkres_arr)
+	if len(jkres_arr) == 0 {
+		ch <- JokeResult{Error: errors.New("bad array length 0")}
+		os.Exit(1)
+	}
 
 	// put result struct into the channel - the asynchronous way of returning(?)
 	ch <- jkres_arr[0]
 }
 
+// TODO: figure out what to do with joke_type.. maybe make it into an array?
 // get n non-repeating jokes, concurrently
-func GetRandomJokes(ch chan JokeResult, n int, joke_type string) []JokeResult{
-	// TODO: figure out what to do with joke_type.. maybe make it into an array?
-	
+func GetRandomJokes(num_jokes int, joke_type string) ([]JokeResult, error){
+	// parameters error checking
+	if joke_type != "programming" && joke_type != "general" {
+		return nil, errors.New("Bad joke type! Shold be programming or general")
+	}
+	if num_jokes < 0 {
+		return nil, errors.New("Invalid num_jokes value!")
+	}
+
+	// create a channel
+	ch := make(chan JokeResult, num_jokes)
+	// return value
+	jkres_list := make([]JokeResult, num_jokes)
 	// thread-safe dict
 	cache := Cache{dict: make(map[int]bool)}
 
-	// return value
-	jkres_list := make([]JokeResult, n)
-
-	// breaks only when gathered n non-repeaing jokes. A better way to write this?
+	// asynchrounously call GetARandomJoke. n non-repeaing jokes. A better way to write this?
 	cnt := 0
 	for {
-		if cnt == n {
+		if cnt == num_jokes {
 			break
 		}
 
@@ -116,5 +134,5 @@ func GetRandomJokes(ch chan JokeResult, n int, joke_type string) []JokeResult{
 			cnt += 1
 		}
 	}
-	return jkres_list
+	return jkres_list, nil
 }
