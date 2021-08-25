561591c3b90551d6005fd50456bd5494a071e203
jialuwei <jialu.wei@xandr.com>
2021-08-25 07:58:04 -0400

    now with a loop added

--- demo/demo.go
+++ demo/demo.go
@@ -8,12 +8,8 @@ import (
 	"errors"
 )
 
-// input: take number n from command line
-// output: a list of n jokes: Setup and punchline (format tbd)
-func main() {
 
-	var num_jokes int
-	var joke_type_int int
+func main() {
 	jkmap := map[int]string{
 		1: "general",
 		2: "programming",
@@ -22,40 +18,57 @@ func main() {
 		5: "Christmas",
 	}
 
-	// get joke type
-	fmt.Println("What kind of joke would you like?")
-	fmt.Println("Select joke category:  1) General 2) Programming 3) Pun 4) Spooky 5) Christmas ")
-	_, err := fmt.Scanf("%d", &joke_type_int)
-	joke_type, ok := jkmap[joke_type_int]
-	if err != nil || !ok {
-		fmt.Print(err.Error()) // TODO: customize error
-		os.Exit(1)
+	for {
+
+		// get joke type
+		fmt.Println("What kind of joke would you like?")
+		fmt.Println("Select joke category:  1) General 2) Programming 3) Pun 4) Spooky 5) Christmas ")
+		var joke_type_int int
+		_, err := fmt.Scanf("%d", &joke_type_int)
+		joke_type, ok := jkmap[joke_type_int]
+		if err != nil || !ok {
+			fmt.Print(err.Error()) // TODO: bad joke type check!
+			os.Exit(1)
+		}
+
+		// if joke_type != "programming" && joke_type != "general" && joke_type != "Pun" && joke_type != "Christmas" && joke_type != "Spooky"{
+		// 	return nil, errors.New("Bad joke type! Joke type should be one of followwing: programming, general, Christmas, Pun, Spooky")
+		// }
+
+
+		// get joke num
+		fmt.Printf("You've select: %s\n", joke_type)
+		fmt.Printf("Now, how many %s jokes would you like?\n", joke_type)
+		var num_jokes int
+		_, err = fmt.Scanf("%d", &num_jokes)
+		if err != nil {
+			fmt.Print(err.Error())
+			os.Exit(1)
+		}
+		if num_jokes < 0 {
+			fmt.Print(errors.New("Invalid num_jokes value!"))
+			os.Exit(1)
+			// return nil, errors.New("Invalid num_jokes value!")
+		}
+
+
+
+		// api := jokes.JokeResultSite2{}
+		// // todo: change this into an array
+		// ch := make(chan jokes.JokeResult)
+		// go api.GetARandomJoke(ch, "Christmas")
+		// res := <- ch
+		// fmt.Println(res)
+
+
+		jokes, err := GetRandomJokes(num_jokes, joke_type)
+		if err != nil {
+			fmt.Print(err.Error())
+			os.Exit(1)
+		}
+		fmt.Println(jokes)
+
 	}
-
-	// get joke num
-	fmt.Printf("You've select: %s\n", joke_type)
-	fmt.Printf("Now, how many %s jokes would you like?\n", joke_type)
-	_, err = fmt.Scanf("%d", &num_jokes)
-	if err != nil {
-		fmt.Print(err.Error())
-		os.Exit(1)
-	}
-
-	
-	// api := jokes.JokeResultSite2{}
-	// // todo: change this into an array
-	// ch := make(chan jokes.JokeResult)
-	// go api.GetARandomJoke(ch, "Christmas")
-	// res := <- ch
-	// fmt.Println(res)
-
-
-	jokes, err := GetRandomJokes(num_jokes, joke_type)
-	if err != nil {
-		fmt.Print(err.Error())
-		os.Exit(1)
-	}
-	fmt.Println(jokes)
 
 
 }
@@ -63,21 +76,13 @@ func main() {
 
 // get n non-repeating jokes, concurrently
 func GetRandomJokes(num_jokes int, joke_type string) ([]jokes.JokeResult, error){
-	// error checking 
-	if joke_type != "programming" && joke_type != "general" && joke_type != "Pun" && joke_type != "Christmas" && joke_type != "Spooky"{
-		return nil, errors.New("Bad joke type! Joke type should be one of followwing: programming, general, Christmas, Pun, Spooky")
-	}
-	if num_jokes < 0 {
-		return nil, errors.New("Invalid num_jokes value!")
-	}
-
 	ch := make(chan jokes.JokeResult, num_jokes)
 	c := cache.NewCache() // tracks existing ids
 	jkres_list := make([]jokes.JokeResult, num_jokes) // return value
+	
 	api := jokes.JokeRouter(joke_type) // find correct struct to call based on joke_type
 
 
-	// asynchrounously call GetARandomJoke. n non-repeaing jokes. A better way to write this?
 	cnt := 0
 	for {
 		if cnt == num_jokes {
